f87c15f39556d4284933b9f5ec0ca9138105c703
mehrad <mehradj@yahoo.com>
2022-06-07 22:03:29 +0430

    getter setter added for Course

--- Course.h
+++ Course.h
@@ -11,6 +11,31 @@ class Course{
         Course(Course&);
         ~Course();
 
+        void setName(string name){
+            this->name = name;
+        }
+
+        string getName(){
+            return name;
+        }
+
+        void setUnit(int unit){
+            this->unit = unit;
+        }
+
+        int getUnit(){
+            return unit;
+        }
+
+        void setMark(double *mark){
+            this->mark = mark;
+        }
+
+        double* getMark(){
+            return mark;
+        }
+
+
     private:
         string name;
         int unit;
