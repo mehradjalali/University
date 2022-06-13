1e9d5971836ed175282bbf46a39b1724a5adc654
mehrad <mehradj@yahoo.com>
2022-06-13 15:07:02 +0430

    cmake added

---
+++ CMakeFiles/University.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/amin/Documents/ap/jungle/second/Course.cpp" "/home/amin/Documents/ap/jungle/second/CMakeFiles/University.dir/Course.cpp.o"
+  "/home/amin/Documents/ap/jungle/second/Person.cpp" "/home/amin/Documents/ap/jungle/second/CMakeFiles/University.dir/Person.cpp.o"
+  "/home/amin/Documents/ap/jungle/second/Professor.cpp" "/home/amin/Documents/ap/jungle/second/CMakeFiles/University.dir/Professor.cpp.o"
+  "/home/amin/Documents/ap/jungle/second/Student.cpp" "/home/amin/Documents/ap/jungle/second/CMakeFiles/University.dir/Student.cpp.o"
+  "/home/amin/Documents/ap/jungle/second/University.cpp" "/home/amin/Documents/ap/jungle/second/CMakeFiles/University.dir/University.cpp.o"
+  "/home/amin/Documents/ap/jungle/second/main.cpp" "/home/amin/Documents/ap/jungle/second/CMakeFiles/University.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/University.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/University.dir/Course.cpp.o"
+  "CMakeFiles/University.dir/Person.cpp.o"
+  "CMakeFiles/University.dir/Professor.cpp.o"
+  "CMakeFiles/University.dir/Student.cpp.o"
+  "CMakeFiles/University.dir/University.cpp.o"
+  "CMakeFiles/University.dir/main.cpp.o"
+  "University"
+  "University.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/University.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
