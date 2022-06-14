f2ee8f534d92218a3c8a3621efffecacea38f51c
mehradj <mehradj@yahoo.com>
2022-06-14 13:54:39 +0430

    operator>> overloaded for Student

--- main.cpp
+++ main.cpp
@@ -1,34 +1,39 @@
 #include "Course.h"
 #include "Person.h"
-#include "Student.h"
 #include "Professor.h"
+#include "Student.h"
 #include "University.h"
 
-ostream& operator<<(ostream& os, Course& first){
-    os << "name: " << first.getName() << '\n' << "unit: " << first.getUnit() << '\n' << "mark: " << first.getMark();
+ostream &operator<<(ostream &os, Course &first) {
+    os << "name: " << first.getName() << '\n'
+       << "unit: " << first.getUnit() << '\n'
+       << "mark: " << first.getMark();
     return os;
 }
 
-ostream& operator<<(ostream& os, Person& first){
-    os << "firstName: " << first.getFirstName() << '\n' << "lastName: ";
-    os << first.getLastName() << '\n' << "id: " << first.getId() << '\n' << "workHours: " << first.getWorkHours() << '\n';
+ostream &operator<<(ostream &os, Person &first) {
+    os << "firstName: " << first.getFirstName() << '\n'
+       << "lastName: ";
+    os << first.getLastName() << '\n'
+       << "id: " << first.getId() << '\n'
+       << "workHours: " << first.getWorkHours() << '\n';
     return os;
 }
 
-ostream& operator<<(ostream& os, Student& first){
-    os << (Person&) first;
+ostream &operator<<(ostream &os, Student &first) {
+    os << (Person &)first;
     os << "field of study: " << first.getFieldOfStudy() << '\n';
     os << "number of courses: " << first.getNumOfCourses() << '\n';
     return os;
 }
 
-ostream& operator<<(ostream& os, Professor& first){
-    os << (Person&) first;
+ostream &operator<<(ostream &os, Professor &first) {
+    os << (Person &)first;
     os << "title: " << first.titles[first.getTitle()] << '\n';
     return os;
 }
 
-ostream& operator<<(ostream& os, University& first){
+ostream &operator<<(ostream &os, University &first) {
     first.sort(*first.professors, first.numOfProfessors);
     first.sort(*first.students, first.numOfStudents);
     os << "Professors: \n";
@@ -37,31 +42,45 @@ ostream& operator<<(ostream& os, University& first){
     os << "Students: \n";
     for (int i = 0; i < first.numOfStudents; i++)
         os << first.students[i]->getFirstName() << " " << first.students[i]->getLastName() << endl;
+    return os;
 }
 
-istream& operator>>(istream& is, Course& first){
+istream &operator>>(istream &is, Course &first) {
     cout << "Enter name: ";
     is >> first.name;
     cout << "\nEnter unit: ";
     is >> first.unit;
     cout << "Enter mark: ";
     is >> *first.mark;
+    return is;
 }
 
-istream& operator>>(istream&is, Person& first){
+istream &operator>>(istream &is, Person &first) {
     cout << "Enter first name: ";
     is >> first.firstName;
     cout << "Enter last name: ";
     is >> first.lastName;
-    cout << "id";
+    cout << "Enter id";
     is >> first.id;
     if (!first.validate(first.id))
         first.error("invalid id");
     cout << "workHours: ";
     is >> *first.workHours;
+    return is;
 }
 
-int main(){
+istream &operator>>(istream &is, Student &first) {
+    is >> (Person &)first;
+    cout << "Enter field of study: ";
+    is >> first.fieldOfStudy;
+    cout << "Enter number of courses: ";
+    is >> first.numOfCourses;
+    for (int i = 0; i < first.numOfCourses; i++)
+        is >> first.getCourse(i);
+    
+}
+
+int main() {
     Course c[2];
     double a = 20, a1 = 19, a2 = 7;
     c[0] = Course("salam", 4, &a);
