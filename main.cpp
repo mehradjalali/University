391778344c651d958909edf0fa9712bb725db20a
mehradj <mehradj@yahoo.com>
2022-06-14 21:29:44 +0430

    tested all functions in main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,86 @@
 #include "Student.h"
 #include "University.h"
 
+#define err(x) cout << #x << ":\n" << x << "\n\n"
+
+int main() {
+    University uni0;
+    cin >> uni0; // >> operator for University, Course, Student and Professor
+    err(uni0); // << operator for University
+    
+    Course crs0("Calculus 2", 3, 12.3); // Constructor
+    err(crs0); // << operator for Course
+
+    Course crs1 = uni0.students[3]->getCourse(1); // Copy Constructor
+    err(crs1);
+
+    crs1.setMark(18.75);
+    err(crs1.getMark());
+    
+    crs1.setName("Advanced Programming");
+    err(crs1.getName());
+    
+    crs1.setUnit(3);
+    err(crs1.getUnit());
+
+    // Person p0("Mehrad", "Jalali", "15meh12378", 6.0); // exit(0) becase 15meh12378 is not a valid id
+    Person p0("Mehrad", "Jalali", "00meh12378", 6.0); // Constructor
+    err(p0); // << operator for Person
+    
+    Person p1 = p0; // Copy Constructor
+    err(p1);
+    
+    p1.setFirstName("Amin");
+    err(p1.getFirstName());
+
+    p1.setLastName("Zolfagharian");
+    err(p1.getLastName());
+    
+    p1.setId("00ami32198");
+    err(p1.getId());
+    
+    p1.setWorkHours(8.0);
+    err(p1.getWorkHours());
+    err(p1.calculateSalary());
+    
+    p1 = p0; // oprator=
+    err(p1);
+    // p1.setId("82ami12345"); // exit(0) because 82ami12345 is not a valid id
+    
+    Course aslCourses[2] = {crs0, crs1};
+    
+    Student stu0("Hadi", "Asl Ghasemi", "00*sl12378", 3.0, aslCourses, "Computer Engineering", 2); // Constructor
+    err(stu0); // << operator for Student
+    
+    Student stu1 = stu0; // Copy Constructor
+    err(stu1.gpa());
+    err(stu1.calculateSalary());
+    
+    Professor prf0("Jeff", "Costello", "92#12389", 8.0, ProfessorTitle(1)); // Constructor
+    err(prf0); // << operator for Professor
+
+    Professor prf1 = prf0; // Copy Constructor
+    err(prf1);
+
+    prf1.setTitle(ProfessorTitle(0));
+    err(prf1.getTitle());
+
+    prf1.setWorkHours(7.0);
+    err(prf1.getWorkHours());
+    err(prf1.calculateSalary());
+
+
+    University uni1 = uni0; // Copy Constructor
+    err(uni1);
+    
+    err(uni1.averageGpa());
+    err(uni1.averageGpaOfField("Computer Engineering"));
+    err(uni1.averageMarkOfCourse("Advanced Programming"));
+    err(uni1.isEnoughBudget());
+    uni1.printCourses();
+    uni1.saveToFile();
+}
+
 ostream &operator<<(ostream &os, Course &first) {
     os << "name: " << first.getName() << '\n'
        << "unit: " << first.getUnit() << '\n'
@@ -122,13 +202,4 @@ istream &operator>>(istream &is, University &first){
         is >> temp2;
         first.setStudent(i, temp2);
     }
-}
-
-int main() {
-    Course c[2];
-    double a = 20, a1 = 19, a2 = 7;
-    c[0] = Course("salam", 4, &a);
-    c[1] = Course("dada", 3, &a1);
-    Professor s("mehrad", "jalali", "98#77777", &a2, PROFESSOR);
-    cout << s;
 }
